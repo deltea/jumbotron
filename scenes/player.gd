56be85921128020151cb926291cc772f14ec52ec
deltea <thcheetah777@gmail.com>
2026-01-31 17:43:43 -0800

    move dynamics updating to normal process cuz im a dumass

--- scenes/player.gd
+++ scenes/player.gd
@@ -8,23 +8,25 @@ extends CharacterBody2D
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var bat: Node2D = $Bat
+@onready var bat_sprite: Sprite2D = $Bat/Sprite2D
 
 @onready var bat_pos_dynamics: DynamicsSolverVector = Dynamics.create_dynamics_vector(8, 1, 2)
-@onready var bat_rot_dynamics: DynamicsSolver = Dynamics.create_dynamics(8, 1, 2)
+@onready var bat_rot_dynamics: DynamicsSolver = Dynamics.create_dynamics(4, 0.4, 2)
 
 var target_bat_rot = bat_rot_offset
 
 func _ready() -> void:
-	bat_pos_dynamics.set_value(position)
+	print(bat_rot_dynamics.f, bat_rot_dynamics.z, bat_rot_dynamics.r)
+
+func _process(dt: float) -> void:
+	var mouse_angle = get_angle_to(get_global_mouse_position()) + PI/2
+	bat.position = bat_pos_dynamics.update(global_position)
+	bat.rotation = mouse_angle + bat_rot_dynamics.update(deg_to_rad(target_bat_rot))
 
 func _physics_process(dt: float) -> void:
 	var input = Input.get_vector("left", "right", "up", "down")
 	velocity = input.normalized() * move_speed
 	sprite.rotation_degrees = sin(Clock.time * walk_tilt_speed * dt) * walk_tilt if input else 0.0
-
-	var mouse_angle = get_angle_to(get_global_mouse_position()) + PI/2
-	bat.position = bat_pos_dynamics.update(global_position)
-	bat.rotation = mouse_angle + bat_rot_dynamics.update(deg_to_rad(target_bat_rot))
 
 	if Input.is_action_just_pressed("lmb"):
 		swing_bat()
